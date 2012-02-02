5a7adc64528b3aa8806b45d58008b5d7d00b742f
moskvicheff@gmail.com <moskvicheff@gmail.com@952e3234-bb08-de65-9ef6-27c9ed192632>
2012-02-02 21:15:56 +0000

    Brain is fucked

--- trunk/smart_house/cHouse.h
+++ trunk/smart_house/cHouse.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "vector"
 #include "cFloor.h"
+#include "string"
 using namespace std;
 
 class cHouse
 {
 public:
 	cHouse();
+	cHouse(const cHouse &copy);
 	~cHouse();
-
 	vector <cFloor> m_pFloors;
-	
+	char* Name;
+
 	void AddFloor(cFloor &floor);
 	void AddFloor();
 	void Print();
--- trunk/smart_house/main.cpp
+++ trunk/smart_house/main.cpp
@@ -7,9 +7,12 @@
 
 using namespace std;
 
+vector<cHouse> rHouses;
+bool bNameIsNotAvailable = false;
 
 int main ()
 {
+//	rHouses.reserve(sizeof(cHouse)*6);
 
 	/* cHouse house;
 	house.AddFloor();
@@ -27,14 +30,65 @@ int main ()
 	char  string[20];
 	
 	cout << "\nWelcome to your Smart House Manager\n";
-
+	cout << ">";
 	while ( ((cin.getline (string, 20)) && ((strcmp (string, "exit")) != 0) ))
 	{
-		if (((strcmp (string, "create")) == 0))
+		if (((strcmp (string, "add house")) == 0))
+		{			
+			char* Name = new char [10];
+			cout << "\nEnter your house name\n" << ">";
+			cin.getline (Name, 10);
+
+			while ((strcmp (Name, "") == 0))
+				{
+					cout << "\nEnter your house name\n" << ">";
+					cin.getline (Name, 10);
+				}
+
+			if (rHouses.size() != 0)
+			{
+				bNameIsNotAvailable = false;
+				for (size_t i = 0; i < rHouses.size(); ++i)
+				{
+					if (strcmp (rHouses[i].Name, Name) == 0)
+					{
+						cout << "\nThere's already a house with the same name\n";
+						bNameIsNotAvailable = true;
+					}
+
+				}
+			}
+
+			if (!bNameIsNotAvailable)
+			{
+				cHouse house;
+				house.Name = Name;
+				rHouses.push_back(house);
+				cout << "\nYou've created a House\n" << endl;
+				rHouses[rHouses.size()-1].Print();
+			}
+		}
+
+		cout << ">";
+
+		if (((strcmp (string, "delete house")) == 0))
 		{
-			cHouse house;
-			cout << "\nYou've created a House\n" << endl;
-			house.Print();
+			cout << "\nEnter the house name you want to delete\n>";
+			char* Name = new char [10];
+			cin.getline (Name, 10);
+			if (rHouses.size() != 0)
+			{
+				for (size_t i = 0; i < rHouses.size(); ++i)
+				{
+					if (strcmp (rHouses[i].Name, Name))
+					{
+						rHouses.erase(rHouses.begin()+i);
+					}
+
+				}
+			}
+
+
 		}
 	}
 
