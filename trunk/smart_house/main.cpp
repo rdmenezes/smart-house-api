579a985db9d4b3f3e5d34613221355934f9e4bba
moskvicheff@gmail.com <moskvicheff@gmail.com@952e3234-bb08-de65-9ef6-27c9ed192632>
2012-02-02 13:26:14 +0000

    ui

--- trunk/smart_house/main.cpp
+++ trunk/smart_house/main.cpp
@@ -1,5 +1,5 @@
 #include "iostream"
-#include "string"
+#include "cstring"
 #include "cHouse.h"
 #include "cWindow.h"
 #include "cRoom.h"
@@ -11,7 +11,7 @@ using namespace std;
 int main ()
 {
 
-	cHouse house;
+	/* cHouse house;
 	house.AddFloor();
 	house.AddFloor();
 	house.m_pFloors[0].AddRoom();
@@ -22,9 +22,21 @@ int main ()
 	house.m_pFloors[0].m_pRooms[1].AddWindow();
 	house.m_pFloors[1].m_pRooms[0].AddWindow();
 	house.m_pFloors[0].m_pRooms[1].m_pWindows[0].Open();
-
+	house.Print(); */
 	
-	house.Print();
+	char  string[20];
+	
+	cout << "\nWelcome to your Smart House Manager\n";
+
+	while ( ((cin.getline (string, 20)) && ((strcmp (string, "exit")) != 0) ))
+	{
+		if (((strcmp (string, "create")) == 0))
+		{
+			cHouse house;
+			cout << "\nYou've created a House\n" << endl;
+			house.Print();
+		}
+	}
 
 	system ("pause"); 
 }
