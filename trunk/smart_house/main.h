c398651dee7867ff57a5eca1e3eb817e6abc7c3e
moskvicheff@gmail.com <moskvicheff@gmail.com@952e3234-bb08-de65-9ef6-27c9ed192632>
2012-02-20 20:47:29 +0000

    Socket refactoring

--- trunk/smart_house/main.h
+++ trunk/smart_house/main.h
@@ -4,6 +4,8 @@
 #include "cRoom.h"
 #include "cFloor.h"
 #include "cString.h"
+#include "string"
+#include "winsock2.h"
 
 using namespace std;
 
@@ -135,4 +137,50 @@ void PrintHelp()
 		"\"delete house\" - deletes a house; " << endl <<
 		"\"print\" - prints all information about your house's configuration" << endl <<
 		"\"exit\" - exit from the application " << endl;
+}
+
+std::string GetStringFromSocket ()
+{
+	char buffer[1024];
+	int ServerSocket;
+	WORD SocketVersion = MAKEWORD(2,2);
+	WSAStartup (SocketVersion,(WSADATA *) &buffer[0]);
+	
+	ServerSocket = socket (AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in LocalAddr;
+	LocalAddr.sin_family = AF_INET;
+	LocalAddr.sin_port = htons(1234);
+	LocalAddr.sin_addr.s_addr = 0;
+
+	int result = bind(ServerSocket,(sockaddr *) &LocalAddr, sizeof(LocalAddr));
+	int result1 = listen(ServerSocket, 100);
+
+	SOCKET ClientSocket;
+    sockaddr_in ClientSocketAddr;
+	int ClientSocketAddrSize = sizeof(ClientSocketAddr);
+	ClientSocket=accept(ServerSocket, (sockaddr *) &ClientSocketAddr, &ClientSocketAddrSize);
+	char str[1024];
+
+	char buff[1024];
+	std::string string;
+	int Bites_receaved = recv(ClientSocket,&buff[0],sizeof(buff),0);
+	for (size_t i =0; i <= Bites_receaved+1; ++i)
+	{
+		if (i<=Bites_receaved)
+		{
+			str[i] = buff[i];
+		}
+		else str[i-1] = '0';
+	}
+	
+	for (size_t i = 0; i < sizeof(buff);i++)
+	{
+		if (str[i] != '0')
+		{
+			string.push_back(str[i]);
+		}
+		else break;
+	}
+	return string;
 }
